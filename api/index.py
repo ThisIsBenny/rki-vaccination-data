6c8fbafafff37f127bec41821ffb99ac26274020
Benny Samir Hierl <bennysamir@posteo.de>
2020-12-29 21:11:38 +0100

    small change
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- api/index.py
+++ api/index.py
@@ -86,6 +86,7 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.send_header('Cache-Control', 's-maxage=7200, stale-while-revalidate')
+    self.send_header('X-API', 'https://vercel.com/thisisbenny/rki-vaccination-data')
     self.send_header('Content-Type', 'application/json')
     self.end_headers()
     self.wfile.write(json.dumps(res).encode())
