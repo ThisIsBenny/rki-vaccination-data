840ae10b23e25804a0ba7f33800f3a454c64b86f
Benny Samir Hierl <bennysamir@posteo.de>
2020-12-30 13:03:31 +0100

    small change
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- api/index.py
+++ api/index.py
@@ -93,7 +93,6 @@ res = {
 print(res)
 
 class handler(BaseHTTPRequestHandler):
-
   def do_GET(self):
     self.send_response(200)
     self.send_header('X-API', 'https://vercel.com/thisisbenny/rki-vaccination-data')
