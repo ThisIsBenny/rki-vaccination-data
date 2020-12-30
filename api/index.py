549f2548960de8fa0d69b5c306ab7feacc66e75e
Benny Samir Hierl <bennysamir@posteo.de>
2020-12-30 13:02:38 +0100

    remove headers from code
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- api/index.py
+++ api/index.py
@@ -96,7 +96,6 @@ class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     self.send_response(200)
-    self.send_header('Cache-Control', 's-maxage=7200, stale-while-revalidate')
     self.send_header('X-API', 'https://vercel.com/thisisbenny/rki-vaccination-data')
     self.send_header('Content-Type', 'application/json')
     self.end_headers()
