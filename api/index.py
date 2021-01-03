e87fd6d6a71720a35317fd782cef79c5ceddbf04
Benny <ThisIsBenny@users.noreply.github.com>
2021-01-03 20:16:08 +0100

    Access-Control-Allow-Origin added

--- api/index.py
+++ api/index.py
@@ -96,6 +96,7 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     self.send_response(200)
     self.send_header('Content-Type', 'application/json')
+    self.send_header('Access-Control-Allow-Origin', '*')
     self.end_headers()
     self.wfile.write(json.dumps(res).encode())
     return
