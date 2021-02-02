4a73ac449533d443fcb47a0af19dab60f3f7764f
Benny Samir Hierl <bennysamir@posteo.de>
2021-02-02 19:22:05 +0100

    tests added
    Signed-off-by: Benny Samir Hierl <bennysamir@posteo.de>

--- api/tests/scrap_data_test.py
+++ api/tests/scrap_data_test.py
@@ -7,6 +7,9 @@ class TestModule(unittest.TestCase):
   def test_get_file(self):
     """ Test for scrap_data.get_file """
     self.assertTrue(type(scrap_data.get_file()).__name__ == "bytes")
+  def test_get_data(self):
+    """ Test for scrap_data.get_data """
+    self.assertTrue(type(scrap_data.get_data()).__name__ == "dict")
 
 if __name__ == '__main__':
   unittest.main()
